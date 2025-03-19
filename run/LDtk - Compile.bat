dcad457e75fe361d2808febe100edd5c0e101116
Caio Raphael <caioraphael00@gmail.com>
2025-03-19 12:51:47 -0300

    attempt to update the schema

--- run/LDtk - Compile.bat
+++ run/LDtk - Compile.bat
@@ -2,22 +2,30 @@
 cd /d "%~dp0"/..
 
 echo Compilando main.debug.hxml...
+
 haxe main.debug.hxml
 if %errorlevel% neq 0 (
-    echo Erro ao compilar main.debug.hxml.
+    echo.
+    echo Erro ao compilar.
+    echo.
     pause
     exit /b %errorlevel%
 )
-echo "> Sucesso."
 
+echo.
+echo ^> Sucesso.
+echo.
 
 echo Compilando renderer.debug.hxml...
 haxe renderer.debug.hxml
 if %errorlevel% neq 0 (
-    echo Erro ao compilar renderer.debug.hxml.
+    echo.
+    echo Erro ao compilar.
+    echo.
     pause
     exit /b %errorlevel%
 )
-echo "> Sucesso."
+echo.
+echo ^> Sucesso.
 
 pause
--- run/LDtk - Run.bat
+++ run/LDtk - Run.bat
@@ -2,9 +2,12 @@
 cd /d "%~dp0"/../app
 
 echo Iniciando o programa...
+
 npm run start
 if %errorlevel% neq 0 (
+    echo.
     echo Erro ao executar npm run start.
+    echo.
     pause
     exit /b %errorlevel%
 )
